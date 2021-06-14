f0021bd503e25cce5ef01e706cf18493addfaa19
nobel-noba <nobelmitiku@gmail.com>
2021-06-14 13:49:40 +0300

    adding the update and deleting functionalities

---
+++ test/delete.test.js
@@ -0,0 +1,40 @@
+// mocha is actualy used for the teesting of whether the CRUD operation is done or note
+
+const assert= require('assert');
+const MarioChar= require('../models/mariocar');
+
+
+
+// Describe test
+describe('Deleting Record',function(){
+
+    var char;
+
+    beforeEach(function(done){
+        char= new MarioChar({
+            name:'mario',
+            waight:16
+        });
+
+        char.save().then(function(){
+            assert(char.isNew===false);
+            done();
+        });
+    });
+
+    // create tests
+    it('delete a record from the database',function(done){
+        MarioChar.findOneAndRemove({
+            name:'mario'
+        }).then(function(){
+            MarioChar.findOne({name:'mario'}).then(function(result){
+                assert(result===null);
+                done();
+            });
+        });
+
+    });
+    
+    //next test
+
+});
